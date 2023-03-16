740c2c60397355efaa825f797ea0f6b044ed5697
Unam <97320894+manukao@users.noreply.github.com>
2023-03-16 12:18:45 +0100

    change margin from 20px to 2rem

--- components/BoxWithClassName/BoxWithStyledComponents.js
+++ components/BoxWithClassName/BoxWithStyledComponents.js
@@ -4,7 +4,7 @@ const StyledBox = styled.div`
   width: 100px;
   height: 100px;
   background-color: ${(props) => (props.isBlack ? "black" : "green")};
-  margin: 20px;
+  margin: 2rem;
   &:hover {
     background-color: red;
   }
